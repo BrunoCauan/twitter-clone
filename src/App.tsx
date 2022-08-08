1b4242c22d37e4a6e949d9c1dc65fed15d4415b7
Your NameBrunoCauan <brunocaua2000@gmail.com>
2022-08-08 16:09:37 -0300

    WIP check for logged user

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+import { Login } from './pages/Login/Login.page';
+import { Register } from './pages/Register/Register.page';
 import { Nav } from './components/Nav/Nav.component';
 import { Home } from './pages/Home/Home.page';
 import { TweetDetail } from './pages/TweetDetail/TweetDetail.page';
@@ -9,23 +11,45 @@ import { Messages } from './pages/Messages/Messages.page';
 
 import { AppComponent, MainContainer } from './App.styles';
 
+const isLoggedIn: boolean = true;
+
 export function App() {
     return (
         <BrowserRouter>
-            <AppComponent>
-                <Nav />
-
-                <MainContainer>
-                    <Routes>
-                        <Route path='*' element={<Navigate to='home' />} />
-                        <Route path='/home' element={<Home />} />
-                        <Route path=':userName/tweets/:tweetId' element={<TweetDetail />} />
-                        <Route path=':userName' element={<Profile />} />
-                        <Route path='/notifications' element={<Notifications />} />
-                        <Route path='/messages' element={<Messages />} />
-                    </Routes>
-                </MainContainer>
-            </AppComponent>
+            {getRoutes()}
         </BrowserRouter>
     );
+}
+
+function getRoutes() {
+    if (isLoggedIn) {
+        return AuthorizedRoutes();
+    }
+    
+    return (
+        <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='*' element={<Navigate to='login' />} />
+        </Routes>
+    )
+}
+
+function AuthorizedRoutes() {
+    return (
+        <AppComponent>
+            <Nav />
+
+            <MainContainer>
+                <Routes>
+                    <Route path='/home' element={<Home />} />
+                    <Route path=':userName/tweets/:tweetId' element={<TweetDetail />} />
+                    <Route path=':userName' element={<Profile />} />
+                    <Route path='/notifications' element={<Notifications />} />
+                    <Route path='/messages' element={<Messages />} />
+                    <Route path='*' element={<Navigate to='home' />} />
+                </Routes>
+            </MainContainer>
+        </AppComponent>
+    )
 }
