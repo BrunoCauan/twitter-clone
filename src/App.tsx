af952d729efa6118bba1e1455a3e529184fbfdd4
Your NameBrunoCauan <brunocaua2000@gmail.com>
2022-08-08 10:52:46 -0300

    WIP Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,63 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import styled from 'styled-components';
 
-import { Home } from "./pages/Home.page";
-import { TweetDetail } from "./pages/TweetDetail.page";
+import { Home } from './pages/Home.page';
+import { TweetDetail } from './pages/TweetDetail.page';
+import { Nav } from './components/Nav.component';
 
 export function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Home />} />
-                <Route path=":userName/tweet/:tweetId" element={<TweetDetail />} />
-            </Routes>
-        </BrowserRouter>
+        <AppComponent>
+            <Nav />
+
+            <MainContainer>
+                <BrowserRouter>
+                
+                <Routes>
+                    <Route path='/' element={<Navigate to='home' />} />
+                    <Route path='home' element={<Home />} />
+                    <Route path=':userName/tweets/:tweetId' element={<TweetDetail />} />
+                </Routes>
+            </BrowserRouter>
+            </MainContainer>
+        </AppComponent>
     );
 }
+
+const AppComponent = styled.div`
+    min-height: 100vh;
+    height: 100%;
+    display: flex;
+`;
+
+const MainContainer = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    flex: auto;
+    flex-grow: 2;
+
+    & > * {
+        border-left: 1px solid #38444d;
+        border-right: 1px solid #38444d;
+        width: 100%;
+        max-width: 600px;
+        height: 100%;
+
+        & > * {
+            padding-left: 16px;
+            padding-right: 16px;
+        }
+    }
+
+    header {
+        display: flex;
+        align-items: center;
+        height: 53px;
+        background-color: #15202bbf;
+        backdrop-filter: blur(12px);
+        position: sticky;
+        top: 0;
+        left: 0;
+    }
+`;
--- src/components/Tweet.component.tsx
+++ src/components/Tweet.component.tsx
@@ -1,6 +1,7 @@
 /// <reference types="vite-plugin-svgr/client" />
 
 import styled from 'styled-components';
+import { Link } from "react-router-dom";
 
 import { TweetModel } from '../models/Tweet.model';
 import { Avatar } from './Avatar.component';
@@ -16,56 +17,61 @@ interface TweetProps {
 export function Tweet({ tweet }: TweetProps) { 
     return (
         <TweetContainer>
-            <Avatar user={tweet.author}/>
-            
-            <TweetContent>
-                <TweetHeader>
-                    <a href={`profile/${tweet.author.userName}`}>
-                        {tweet.author.name}
-                    </a>
-
-                    <span className='heading-info'>
-                        @{ tweet.author.userName } · { tweet.time.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' }) }
-                    </span>
-                </TweetHeader>
+            <StyledLink to={`/${tweet.author.userName}/tweets/${tweet.id}`}>
+                <Avatar user={tweet.author}/>
                 
-                <TweetText>{tweet.text}</TweetText>
-            
-                <TweetFooter>
-                    <TweetAction>
-                        <Replies />
+                <TweetContent>
+                    <TweetHeader>
+                        <a href={`profile/${tweet.author.userName}`}>
+                            {tweet.author.name}
+                        </a>
 
-                        {tweet?.repliesCount > 0 && 
-                            <span className='count'>{tweet.repliesCount}</span>                            
-                        }
-                    </TweetAction>
+                        <span className='heading-info'>
+                            @{ tweet.author.userName } · { tweet.time.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' }) }
+                        </span>
+                    </TweetHeader>
                     
-                    <TweetAction>
-                        <Retweets />
+                    <TweetText>{tweet.text}</TweetText>
+                
+                    <TweetFooter>
+                        <TweetAction>
+                            <Replies />
 
-                        {tweet?.retweetsCount > 0 && 
-                            <span className='count'>{tweet.retweetsCount}</span>                            
-                        }
-                    </TweetAction>
-                    
-                    <TweetAction>
-                        <Likes />
+                            {tweet?.repliesCount > 0 && 
+                                <span className='count'>{tweet.repliesCount}</span>                            
+                            }
+                        </TweetAction>
                         
-                        {tweet?.likesCount > 0 && 
-                            <span className='count'>{tweet.likesCount}</span>                            
-                        }
-                    </TweetAction>
-                </TweetFooter>
-            </TweetContent>
+                        <TweetAction>
+                            <Retweets />
+
+                            {tweet?.retweetsCount > 0 && 
+                                <span className='count'>{tweet.retweetsCount}</span>                            
+                            }
+                        </TweetAction>
+                        
+                        <TweetAction>
+                            <Likes />
+                            
+                            {tweet?.likesCount > 0 && 
+                                <span className='count'>{tweet.likesCount}</span>                            
+                            }
+                        </TweetAction>
+                    </TweetFooter>
+                </TweetContent>
+            </StyledLink>
         </TweetContainer>
     )
 };
 
 const TweetContainer = styled.article`
-    display: flex;
     padding-top: 12px;
     padding-bottom: 12px;
     border-bottom: 1px solid #38444d;
+`;
+
+const StyledLink = styled(Link)`
+    display: flex;
 `;
 
 const TweetContent = styled.div``;
